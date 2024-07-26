3c37a701a99262b6090e090e64c6a66a3c9fa206
Samuel Kojo Anafi Antwi <samuelantwi103@gmail.com>
2024-07-26 20:41:18 +0000

    enroll courses 1.1

--- project/src/app/(accounts)/loading.tsx
+++ project/src/app/(accounts)/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   
   return (
-    <div className="flex w-full h-screen justify-center items-center">
-      <div className="w-20 h-20 border-[10px] border-blue-500 rounded-full animate-spin border-t-transparent"></div>
-    </div>
+    <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
   );
 }
--- project/src/utils/student_api.ts
+++ project/src/utils/student_api.ts
@@ -1,105 +1,21 @@
-// // import { api } from '@utils/api.ts';
+import { createContext, ReactNode, useState } from "react";
 
-import axios from "axios";
-import { GetStaticProps } from "next";
+// Define the types for your data
+interface studentType {
+  id: string | null;
+  type: string | null;
+}
 
-// import api from "./api";
+interface AuthContextType {
+  studentInfoStored: studentType;
+  setStudentInfoStored: (studentInfoStored: studentType) => void;
+}
 
+export const AuthInfo = createContext<AuthContextType>({
+  studentInfoStored: {
+    id: "",
+    type: null,
+  },
+  setStudentInfoStored: ({ id, type }) => {},
+});
 
-
-// // Define the types for your data
-// interface Student {
-//   student_id: number;
-//   fname: string;
-//   lname: string;
-//   oname?: string;
-//   email: string;
-//   phone: string;
-//   password: string;
-//   dob: string; // or Date if you handle conversion
-//   profile_img?: string;
-//   level: number;
-// }
-
-// interface Enrollment {
-//   course_id: number;
-//   student_id: number;
-//   enrollment_id: number;
-//   score?: number;
-//   grade?: string;
-//   date_enrolled?: string; // or Date if you handle conversion
-// }
-
-// interface PaymentRecord {
-//   payment_id: number;
-//   student_id: number;
-//   amount: number;
-//   payment_year: number;
-//   reference: 'academic' | 'residential' | 'other';
-//   payment_date?: string; // or Date if you handle conversion
-// }
-
-// interface Grade {
-//   course_id: number;
-//   score: number | null;
-//   grade: string | null;
-//   course_code: string;
-//   course_name: string;
-//   credit_hour: number;
-//   academic_year: number;
-//   semester: number;
-//   description: string;
-//   img: string | null;
-//   lecturer_fname: string;
-//   lecturer_lname: string;
-//   lecturer_oname: string;
-//   lecturer_email: string;
-//   lecturer_phone: string;
-//   lecturer_profile_img: string | null;
-// }
-
-// // API function to get student grades
-// export const getStudentGrades = async (s_id: string) => {
-//   try {
-//     const response = await api.get(`/student/retrieveStudentGrades`, {
-//       params: { s_id }
-//     });
-//     console.log(response);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error retrieving student grades:', error);
-//     throw error;
-//   }
-// };
-
-// // API function to enroll a student
-// export const enrollStudent = async (studentData: Student) => {
-//   try {
-//     const response = await api.post(`/student/enrollStudent`, studentData);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error enrolling student:', error);
-//     throw error;
-//   }
-// };
-
-// // API function to calculate outstanding fees
-// export const calculateOutstandingFees = async (studentID: string, academicYear: string) => {
-//   try {
-//     const response = await api.get(`/student/calculateOutstandingFees`, {
-//       params: { studentID, academicYear }
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error calculating outstanding fees:', error);
-//     throw error;
-//   }
-// };
-
-// // Add more functions as needed for other API endpoints
-// // const grade: Grade = getStudentGrades('11111111').grades;
-
-
-
-
-    
